d81236bb46a5bf39ae370cda941bc7d6c9d9c226
kamzak <58105737+kamzak@users.noreply.github.com>
2020-11-25 13:36:23 +0100

    deploy v.0.1

---
+++ js/oblicz.js
@@ -0,0 +1,67 @@
+function oblicz() {
+    const zaok = 5;
+
+    // Pobranie wartości z pól tekstowych
+    let ileOdlewow = document.getElementById("ileOdlewow").value;
+    let ileOdlewowWR = document.getElementById("ileOdlewowWR").value;
+    let ileWD = document.getElementById("ileWD").value;
+    let wspK = document.getElementById("wspK").value;
+    let paramC = document.getElementById("paramC").value;
+    let uzysk = document.getElementById("uzysk").value;
+    let wspKdlaCZ = document.getElementById("wspKdlaCZ").value;
+    let objOdlewu = document.getElementById("objOdlewu").value;
+    let gestStopu = document.getElementById("gestStopu").value;
+
+    // Funkcja zaokrąglająca
+    function roundTo(value, places){
+        let power = Math.pow(10, places);
+        return Math.round(value * power) / power;
+    }
+
+    /* Obliczenia */
+    
+    // Obliczenie masy surowego odlewu
+    let masaOdlewu = document.getElementById("masaOdlewu");
+    masaOdlewu.innerHTML = roundTo(((gestStopu * objOdlewu) / 1000), 3);
+
+    // Obliczenie masy surowego odlewu z układem zasilającym i wlewowym
+    let masaOdlewuZUW = document.getElementById("masaOdlewuZUW");
+    masaOdlewuZUW.innerHTML = roundTo((masaOdlewu.innerHTML * uzysk), 3);
+
+    // Zalecany czas zalewania
+    let czasZ = document.getElementById("czasZ");
+    czasZ.innerHTML = roundTo((wspKdlaCZ * Math.sqrt(masaOdlewuZUW.innerHTML)), 1);
+
+    // Obliczenie sumy powierzchni przekrojów wlewów doprowadzających
+    let suma = masaOdlewuZUW.innerHTML / (roundTo(czasZ.innerHTML, zaok) * wspK );
+    let sumaPrzWD = document.getElementById("sumaPrzWD");
+    sumaPrzWD.innerHTML = roundTo(suma, 1);
+
+    // Obliczanie przekrojów wlewów doprowadzających
+    let przWD = document.getElementById("przWD");
+    przWD.innerHTML = roundTo((sumaPrzWD.innerHTML/ ileWD), 1)
+
+    // Obliczenie przekroju wlewu rozprowadzającego
+    let przWR = document.getElementById("przWR");
+    przWR.innerHTML = roundTo((1.2 * ileOdlewowWR * sumaPrzWD.innerHTML), 2);
+
+    // Obliczenie przekroju wlewu głównego
+    let przWG = document.getElementById("przWG");
+    przWG.innerHTML = roundTo((1 * ileOdlewow * sumaPrzWD.innerHTML), 1);
+
+    // Obliczenie pojemności zbiornika wlewowego
+    let objZW = document.getElementById("objZW");
+    objZW.innerHTML = roundTo(((masaOdlewuZUW.innerHTML*ileOdlewow*1000)/(gestStopu * czasZ.innerHTML)), 1);
+
+    // Prędkość liniowa podnoszenia się metalu w formie
+    let prLin = document.getElementById("prLin");
+    prLin.innerHTML = roundTo((paramC / czasZ.innerHTML), 2);
+
+    // Ukrycie pierwszej strony
+    let container = document.getElementById("container");
+    container.style.display = "none";
+    
+    // Pokazanie tabeli z wynikami
+    let wynik = document.getElementById("wynik");
+    wynik.style.display = "block";
+}
