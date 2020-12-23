c6eddabfa08c7f62a5697781ad07e16dddec86cb
kamzak <58105737+kamzak@users.noreply.github.com>
2020-12-23 21:53:31 +0100

    Update, przekroje

--- js/oblicz.js
+++ js/oblicz.js
@@ -1,33 +1,24 @@
-    // Licznik dla walidacji danych
-    let count = 0;
-
-// Pobranie wartości z pól tekstowych
-    let ileOdlewow = document.getElementById("ileOdlewow").value;
-    let ileOdlewowWR = document.getElementById("ileOdlewowWR").value;
-    let ileWD = document.getElementById("ileWD").value;
-    let wspK = document.getElementById("wspK").value;
-    let paramC = document.getElementById("paramC").value;
-    let uzysk = document.getElementById("uzysk").value;
-    let wspKdlaCZ = document.getElementById("wspKdlaCZ").value;
-    let objOdlewu = document.getElementById("objOdlewu").value;
-    let gestStopu = document.getElementById("gestStopu").value;
-
 // Pobranie pół tekstowych dla walidacji danych
-
-    let ileOdlewowT = document.getElementById("ileOdlewow");
-    let ileOdlewowWRT = document.getElementById("ileOdlewowWR");
-    let ileWDT = document.getElementById("ileWD");
-    let wspKT = document.getElementById("wspK");
-    let paramCT = document.getElementById("paramC");
-    let uzyskT = document.getElementById("uzysk");
-    let wspKdlaCZT = document.getElementById("wspKdlaCZ");
-    let objOdlewuT = document.getElementById("objOdlewu");
-    let gestStopuT = document.getElementById("gestStopu");
+let ileOdlewowT = document.getElementById("ileOdlewow");
+let ileOdlewowWRT = document.getElementById("ileOdlewowWR");
+let ileWDT = document.getElementById("ileWD");
+let wspKT = document.getElementById("wspK");
+let paramCT = document.getElementById("paramC");
+let uzyskT = document.getElementById("uzysk");
+let wspKdlaCZT = document.getElementById("wspKdlaCZ");
+let objOdlewuT = document.getElementById("objOdlewu");
+let gestStopuT = document.getElementById("gestStopu");
+let fwdT = document.getElementById("fwd");
+let fwrT = document.getElementById("fwr");
+let fwgT = document.getElementById("fwg");
     
+// Licznik dla walidacji danych
+let count = 0;
+
+// Funkcja sprawdzająca poprawność danych
 function test() {
     // Test pola ilości odlewów
     if(document.getElementById("ileOdlewow").value >= 1){
-        count++; 
         ileOdlewowT.setAttribute("class", "poprawnie");
     }
     else { 
@@ -36,7 +27,6 @@ function test() {
 
     // Test pola ilości odlewów zasilanych przez układ
     if(document.getElementById("ileOdlewowWR").value >= 1){
-        count++; 
         ileOdlewowWRT.setAttribute("class","poprawnie");
     }
     else { 
@@ -45,7 +35,6 @@ function test() {
 
     // Test liczby wlewów doprowadzających
     if(document.getElementById("ileWD").value >= 1){
-        count++; 
         ileWDT.setAttribute("class","poprawnie");
     }
     else { 
@@ -54,7 +43,6 @@ function test() {
 
     // Test współczynnika K(pk) 0,20 - 0,45
     if(document.getElementById("wspK").value >= 0.20 && document.getElementById("wspK").value <= 0.45){
-        count++; 
         wspKT.setAttribute("class","poprawnie");
     }
     else { 
@@ -64,7 +52,6 @@ function test() {
     // Test parametru C
     if(document.getElementById("paramC").value > 0)
     {
-        count++; 
         paramCT.setAttribute("class","poprawnie");
     } 
     else { 
@@ -74,7 +61,6 @@ function test() {
     // Test współcznnika k (uzysk)
     if (document.getElementById("uzysk").value > 0)
     {
-        count++; 
         uzyskT.setAttribute("class","poprawnie");
     } 
     else { 
@@ -84,7 +70,6 @@ function test() {
     // Test współczynnika k1 (dla czasu zalewania)
     if (document.getElementById("wspKdlaCZ").value > 0)
     {
-        count++; 
         wspKdlaCZT.setAttribute("class","poprawnie");
     } 
     else { 
@@ -93,8 +78,7 @@ function test() {
 
     // Test objętości surowego odlewu
     if (document.getElementById("objOdlewu").value > 0)
-    {
-        count++; 
+    { 
         objOdlewuT.setAttribute("class","poprawnie");
     } 
     else { 
@@ -104,11 +88,30 @@ function test() {
     // Test gęstości właściwej stopu
     if (document.getElementById("gestStopu").value > 0)
     {
-        count++; 
         gestStopuT.setAttribute("class","poprawnie");
     } 
     else { 
         gestStopuT.setAttribute("class","blad");
+    }
+
+    // Test stosunku powierzchni przekrojów elementów układu wlewowego
+    // Fwd
+    if(document.getElementById("fwd").value >= 2 && document.getElementById("fwd").value <= 6) {
+        fwdT.setAttribute("class", "poprawnie");
+    } else {
+        fwdT.setAttribute("class", "blad");
+    }
+    // Fwr
+    if(document.getElementById("fwr").value >= 1.2 && document.getElementById("fwr").value <= 2) {
+        fwrT.setAttribute("class", "poprawnie");
+    } else {
+        fwrT.setAttribute("class", "blad");
+    }
+    // Fwg
+    if(document.getElementById("fwg").value == 1) {
+        fwgT.setAttribute("class", "poprawnie");
+    } else {
+        fwgT.setAttribute("class", "blad");
     }
 
     // Warunek poprawności danych
@@ -120,7 +123,10 @@ function test() {
         uzyskT.classList.contains("poprawnie") &&
         wspKdlaCZT.classList.contains("poprawnie") &&
         objOdlewuT.classList.contains("poprawnie") &&
-        gestStopuT.classList.contains("poprawnie")){
+        gestStopuT.classList.contains("poprawnie") &&
+        fwdT.classList.contains("poprawnie") &&
+        fwrT.classList.contains("poprawnie")&&
+        fwgT.classList.contains("poprawnie")){
         oblicz();
     }
     else {
@@ -130,7 +136,7 @@ function test() {
 
 
 function oblicz() {
-    const zaok = 5;
+    const zaok = 3;
     console.log("funkcja oblicz");
 
     let ileOdlewow = document.getElementById("ileOdlewow").value;
@@ -142,6 +148,9 @@ function oblicz() {
     let wspKdlaCZ = document.getElementById("wspKdlaCZ").value;
     let objOdlewu = document.getElementById("objOdlewu").value;
     let gestStopu = document.getElementById("gestStopu").value;
+    let fwd = document.getElementById("fwd").value;
+    let fwr = document.getElementById("fwr").value;
+    let fwg = document.getElementById("fwg").value;
 
     // Funkcja zaokrąglająca
     function roundTo(value, places){
@@ -153,41 +162,41 @@ function oblicz() {
 
     // Obliczenie masy surowego odlewu
     let masaOdlewu = document.getElementById("masaOdlewu");
-    masaOdlewu.innerHTML = roundTo(((gestStopu * objOdlewu) / 1000), 2);
+    masaOdlewu.innerHTML = roundTo(((gestStopu * objOdlewu) / 1000), zaok);
 
 
     // Obliczenie masy surowego odlewu z układem zasilającym i wlewowym
     let masaOdlewuZUW = document.getElementById("masaOdlewuZUW");
-    masaOdlewuZUW.innerHTML = roundTo((masaOdlewu.innerHTML * uzysk), 2);
+    masaOdlewuZUW.innerHTML = roundTo((masaOdlewu.innerHTML * uzysk), zaok);
 
     // Zalecany czas zalewania
     let czasZ = document.getElementById("czasZ");
-    czasZ.innerHTML = roundTo((wspKdlaCZ * Math.sqrt(masaOdlewuZUW.innerHTML)), 2);
+    czasZ.innerHTML = roundTo((wspKdlaCZ * Math.sqrt(masaOdlewuZUW.innerHTML)), 1);
 
     // Obliczenie sumy powierzchni przekrojów wlewów doprowadzających
-    let suma = masaOdlewuZUW.innerHTML / (roundTo(czasZ.innerHTML, zaok) * wspK);
+    let suma = masaOdlewuZUW.innerHTML / (czasZ.innerHTML * wspK);
     let sumaPrzWD = document.getElementById("sumaPrzWD");
-    sumaPrzWD.innerHTML = roundTo(suma, 2);
+    sumaPrzWD.innerHTML = roundTo(suma, 1);
 
     // Obliczanie przekrojów wlewów doprowadzających
     let przWD = document.getElementById("przWD");
-    przWD.innerHTML = roundTo((sumaPrzWD.innerHTML/ ileWD), 2)
+    przWD.innerHTML = roundTo((sumaPrzWD.innerHTML/ ileWD), 1)
 
     // Obliczenie przekroju wlewu rozprowadzającego
     let przWR = document.getElementById("przWR");
-    przWR.innerHTML = roundTo((1.2 * ileOdlewowWR * sumaPrzWD.innerHTML), 2);
+    przWR.innerHTML = roundTo(fwr * ileOdlewowWR * (sumaPrzWD.innerHTML/ileOdlewowWR), zaok);
 
     // Obliczenie przekroju wlewu głównego
     let przWG = document.getElementById("przWG");
-    przWG.innerHTML = roundTo((1 * ileOdlewow * sumaPrzWD.innerHTML), 2);
+    przWG.innerHTML = roundTo(fwg * ileOdlewow * (sumaPrzWD.innerHTML/ileOdlewowWR), zaok);
 
     // Obliczenie pojemności zbiornika wlewowego
     let objZW = document.getElementById("objZW");
-    objZW.innerHTML = roundTo(((masaOdlewuZUW.innerHTML*ileOdlewow*1000)/(gestStopu * czasZ.innerHTML)), 2);
+    objZW.innerHTML = roundTo(((masaOdlewuZUW.innerHTML*ileOdlewow*1000)/(gestStopu * czasZ.innerHTML)), 1);
 
     // Prędkość liniowa podnoszenia się metalu w formie
     let prLin = document.getElementById("prLin");
-    prLin.innerHTML = roundTo((paramC / czasZ.innerHTML), 2);
+    prLin.innerHTML = roundTo((paramC / czasZ.innerHTML),zaok);
 
     // Ukrycie pierwszej strony
     let container = document.getElementById("container");
